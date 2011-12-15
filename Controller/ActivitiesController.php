f48438dc4270580a59a58befff7e72c1cba862c9
Richard Kersey <github@razorit.com>
2011-12-15 10:15:34 -0500

    Forcing rename

--- Controller/ActivitiesController.php
+++ Controller/ActivitiesController.php
@@ -1,8 +1,9 @@
 <?php
 class ActivitiesController extends AppController {
 
-	var $name = 'Activities';
-	var $allowedActions = array('index'); // allowed because it returns results and there is no view file, so there is no direct access as of now.
+	public $name = 'Activities';
+	public $uses = 'Activities.Activity';
+	public $allowedActions = array('index'); // allowed because it returns results and there is no view file, so there is no direct access as of now.
 
 	function index($parentForeignKey = null) {
 		$this->Activity->recursive = 0;
