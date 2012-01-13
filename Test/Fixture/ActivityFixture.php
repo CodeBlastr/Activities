edd07eab8fb173fc924641f2c629cb101c2b9dd2
Richard Kersey <github@razorit.com>
2012-01-13 09:00:30 -0500

    Testing and fixing various items.

---
+++ Test/Fixture/ActivityFixture.php
@@ -0,0 +1,24 @@
+<?php
+/* Activity Fixture generated on: 2012-01-13 13:29:23 : 1326461363 */
+
+/**
+ * ActivityFixture
+ *
+ */
+class ActivityFixture extends CakeTestFixture {
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('model' => 'Activity', 'connection' => 'test');
+
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+	);
+}
