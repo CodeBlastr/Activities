f6b6150f2796f16fb1f19915c7334e44b2c716a0
Richard Kersey <github@razorit.com>
2012-04-05 10:07:22 -0700

    dev server commit

--- Controller/ActivitiesController.php
+++ Controller/ActivitiesController.php
@@ -7,7 +7,7 @@ class ActivitiesController extends ActivitiesAppController {
 	public $uses = 'Activities.Activity';
 	public $allowedActions = array('index'); // allowed because it returns results and there is no view file, so there is no direct access as of now.
 
-	function index($parentForeignKey = null) {
+	public function index($parentForeignKey = null) {
 		$this->Activity->recursive = 0;
 		$this->paginate = array(
 			'conditions' => array(
