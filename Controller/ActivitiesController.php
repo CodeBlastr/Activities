ac883d7361f89bee1b6b17afe529fe401c1c328a
Richard Kersey <github@razorit.com>
2012-04-12 12:10:16 -0700

    Activities and Blog Upgrades...
    Updated some code in activities to meet standards better, and added a piece of code that makes a blog post publish date today if it wasn't specified.

--- Controller/ActivitiesController.php
+++ Controller/ActivitiesController.php
@@ -19,20 +19,24 @@ class ActivitiesController extends ActivitiesAppController {
 		return $activities;
 	}
 	
-	/**
-	 * Method to add activities from other plugins
-	 */
+/**
+ * Method to add activities from other plugins
+ */
 	public function record() {
     	if (!empty($this->request->data)) {
     	    $this->request->data['Activity']['user_id'] = $this->Auth->user('id');
     	    $this->request->data['Activity']['foreign_key'] = 0;
-    	    if ($this->Activity->add($this->request->data)) {
-    	        $this->Session->setFlash(__('The activity has been saved', true));
-    	    } else {
-    	        $this->Session->setFlash(__('The activity could not be saved. Please, try again.', true));
+    	    try {
+				$this->Activity->add($this->request->data);
+    	        $this->Session->setFlash(__('The activity has been saved'));
+    	   		$this->redirect($this->referer());
+    	    } catch (Exception $e) {
+    	        $this->Session->setFlash($e->getMessage());
+    	   		$this->redirect($this->referer());
     	    }
-    	    $this->redirect($this->referer());
     	}
+    	$this->Session->setFlash(__('Invalid submission'));
+    	$this->redirect($this->referer());
 	}
 
 }
--- Model/Activity.php
+++ Model/Activity.php
@@ -3,12 +3,11 @@ App::uses('ActivitiesAppModel', 'Activities.Model');
 
 class Activity extends ActivitiesAppModel {
 
-	var $name = 'Activity';
-	var $displayField = 'name';
-	var $actsAs = array('Affiliates.Referrable');
-	var $order = array('Activity.created DESC');
+	public $name = 'Activity';
+	public $displayField = 'name';
+	public $order = array('Activity.created DESC');
 
-	var $belongsTo = array(
+	public $belongsTo = array(
 		'User' => array(
 			'className' => 'Users.User',
 			'foreignKey' => 'user_id',
@@ -18,13 +17,23 @@ class Activity extends ActivitiesAppModel {
 		)
 	);
 	
-	function add($data) {
+	public function __construct($id = false, $table = null, $ds = null) {
+		if (in_array('Affiliates', CakePlugin::loaded())) {
+			$this->actsAs[] = 'Affiliates.Referrable';
+		}		
+    	parent::__construct($id, $table, $ds);		
+    }
+	
+	public function add($data) {
 	    if (!empty($data)) {
 	        if ($this->save($data)) {
 	            return true;
-	        }
-        }
+	        } else {
+				throw new Exception(__('The activity could not be saved. Please, try again.'));
+			}
+        } else {
+			throw new Exception(__('Submitted data was invalid.'));
+		}
 	}
 	
 }
-?>
