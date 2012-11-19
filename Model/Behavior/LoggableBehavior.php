a1d023e955ed4c03a10a45e0cdf90bd3e92577d6
Richard Kersey <richard@razorit.com>
2012-11-19 15:11:52 -0500

    Moved the main action to a separate function so it could be triggered on call, instead of in a call back

--- Model/Behavior/LoggableBehavior.php
+++ Model/Behavior/LoggableBehavior.php
@@ -1,32 +1,53 @@
 <?php
 class LoggableBehavior extends ModelBehavior {
 
-	public $model = '';
-	public $foreignKey = '';
-	public $userData = array();
 	public $settings = array('nameField' => 'name', 'descriptionField' => 'description', 'actionDescription' => 'Activity by', 'userField' => 'user_id', 'parentForeignKey' => 'foreign_key');
 	
-
-	public function setup(&$model, $settings = array()) {
+/**
+ * Setup 
+ * 
+ * @param Model $Model
+ * @param array $settings
+ */
+	public function setup(Model $Model, $settings = array()) {
 		$this->settings = array_merge($this->settings, $settings);
 	}
 	
 /**
- * Callback used to save a log of activities for the respective object.
+ * After Save Callback
+ * 
+ * By default, adding the Loggable behavior will log when a record is created only.
+ * 
+ * @param Model $Model
+ * @param bool $created
  */
-	public function afterSave(&$model, $created) {
+	public function afterSave(Model $Model, $created) {
 		if ($created) {
-			$data['Activity']['name'] = !empty($model->data[$model->alias][$this->settings['nameField']]) ? $model->data[$model->alias][$this->settings['nameField']] : null;
-			$data['Activity']['description'] = !empty($model->data[$model->alias][$this->settings['descriptionField']]) ? $model->data[$model->alias][$this->settings['descriptionField']] : null;
-			$data['Activity']['model'] = $model->alias;
-			$data['Activity']['foreign_key'] = $model->id;
-			$data['Activity']['parent_foreign_key'] = !empty($model->data[$model->alias][$this->settings['parentForeignKey']]) ? $model->data[$model->alias][$this->settings['parentForeignKey']] : null;
-			$data['Activity']['action_description'] = !empty($this->settings['actionDescription']) ? $this->settings['actionDescription'] : null;
-			$data['Activity']['user_id'] = !empty($model->data[$model->alias][$this->settings['userField']]) ? $model->data[$model->alias][$this->settings['userField']] : null;
-			$Activity = ClassRegistry::init('Activity');
-			$Activity->create();
-			$Activity->save($data);
+			$this->triggerLog($Model);
 		}
+	}
+    
+/**
+ * Trigger Log method
+ * 
+ * Create an activity easily by attaching this behavior, and using $this->Model->triggerLog($this, array());
+ * 
+ * @param Model $Model
+ * @param array $settings
+ */
+	public function triggerLog(Model $Model, $settings = array()) {
+		$settings = !empty($settings) ? array_merge($this->settings, $settings) : $this->settings;
+        
+		$data['Activity']['name'] = !empty($Model->data[$Model->alias][$settings['nameField']]) ? $Model->data[$Model->alias][$settings['nameField']] : 'null';
+        $data['Activity']['description'] = !empty($Model->data[$Model->alias][$settings['descriptionField']]) ? $Model->data[$Model->alias][$settings['descriptionField']] : null;
+        $data['Activity']['model'] = $Model->alias;
+        $data['Activity']['foreign_key'] = $Model->id;
+        $data['Activity']['parent_foreign_key'] = !empty($Model->data[$Model->alias][$settings['parentForeignKey']]) ? $Model->data[$Model->alias][$settings['parentForeignKey']] : null;
+        $data['Activity']['action_description'] = !empty($settings['actionDescription']) ? $settings['actionDescription'] : null;
+        $data['Activity']['user_id'] = !empty($Model->data[$Model->alias][$settings['userField']]) ? $Model->data[$Model->alias][$settings['userField']] : null;
+        $Activity = ClassRegistry::init('Activity');
+        $Activity->create();
+        $Activity->save($data);		
 	}
 
 }
